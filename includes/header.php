c38b046c9a49d8d153e1ab8f56fda8893c8689b6
Mosser <155332027+slybepo@users.noreply.github.com>
2024-12-14 11:10:31 +0100

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,12 @@
+<div class="sidebar">
+    <nav>
+        <ul>
+            <li><a href="#">Category 1</a></li>
+            <li><a href="#">Category 2</a></li>
+            <li><a href="#">Category 3</a></li>
+        </ul>
+    </nav>
+    <div class="settings">
+        <a href="settings.php"><img src="/assets/images/settings-icon.png" alt="Settings"></a>
+    </div>
+</div>
