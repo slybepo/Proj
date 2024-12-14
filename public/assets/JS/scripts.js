4c6e3909d8b21db66161f9f8994d45d3c56cdb70
Mosser <155332027+slybepo@users.noreply.github.com>
2024-12-14 20:30:27 +0100

    Delete public directory

--- public/assets/JS/scripts.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.sidebar nav ul li').forEach(item => {
-    item.addEventListener('click', () => {
-        const dropdown = item.querySelector('ul');
-        if (dropdown) dropdown.classList.toggle('show');
-    });
-});
